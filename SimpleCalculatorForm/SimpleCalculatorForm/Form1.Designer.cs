8aaf3fc3ca9e560c99c95dedd59a3b30253ed919
Ice7777 <37283334+Ice7777@users.noreply.github.com>
2020-07-20 14:11:39 +0800

    Add files via upload

---
+++ SimpleCalculatorForm/SimpleCalculatorForm/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace SimpleCalculatorForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.Label01 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt_point = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btBackSpace = new System.Windows.Forms.Button();
+            this.bt_result = new System.Windows.Forms.Button();
+            this.bt_div = new System.Windows.Forms.Button();
+            this.bt_multi = new System.Windows.Forms.Button();
+            this.bt_minus = new System.Windows.Forms.Button();
+            this.bt_plus = new System.Windows.Forms.Button();
+            this.btCE = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(25, 49);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.ReadOnly = true;
+            this.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox1.Size = new System.Drawing.Size(710, 25);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // Label01
+            // 
+            this.Label01.Location = new System.Drawing.Point(22, 17);
+            this.Label01.Name = "Label01";
+            this.Label01.Size = new System.Drawing.Size(363, 29);
+            this.Label01.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.bt3);
+            this.groupBox1.Controls.Add(this.bt6);
+            this.groupBox1.Controls.Add(this.bt9);
+            this.groupBox1.Controls.Add(this.bt2);
+            this.groupBox1.Controls.Add(this.bt5);
+            this.groupBox1.Controls.Add(this.bt8);
+            this.groupBox1.Controls.Add(this.bt7);
+            this.groupBox1.Controls.Add(this.bt4);
+            this.groupBox1.Controls.Add(this.bt1);
+            this.groupBox1.Controls.Add(this.bt_point);
+            this.groupBox1.Controls.Add(this.bt0);
+            this.groupBox1.Location = new System.Drawing.Point(25, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 327);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(281, 180);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(52, 46);
+            this.bt3.TabIndex = 10;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Location = new System.Drawing.Point(281, 107);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(52, 46);
+            this.bt6.TabIndex = 9;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.Location = new System.Drawing.Point(281, 36);
+            this.bt9.Margin = new System.Windows.Forms.Padding(33, 33, 33, 3);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(52, 46);
+            this.bt9.TabIndex = 8;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(162, 180);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(52, 46);
+            this.bt2.TabIndex = 7;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(162, 107);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(52, 46);
+            this.bt5.TabIndex = 6;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.Location = new System.Drawing.Point(162, 35);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(52, 46);
+            this.bt8.TabIndex = 5;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt7
+            // 
+            this.bt7.Location = new System.Drawing.Point(49, 36);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(52, 46);
+            this.bt7.TabIndex = 4;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(49, 107);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(52, 46);
+            this.bt4.TabIndex = 3;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(49, 180);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(52, 46);
+            this.bt1.TabIndex = 2;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt_point
+            // 
+            this.bt_point.Location = new System.Drawing.Point(244, 258);
+            this.bt_point.Name = "bt_point";
+            this.bt_point.Size = new System.Drawing.Size(89, 38);
+            this.bt_point.TabIndex = 1;
+            this.bt_point.Text = ".";
+            this.bt_point.UseVisualStyleBackColor = true;
+            this.bt_point.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.Location = new System.Drawing.Point(49, 258);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(89, 38);
+            this.bt0.TabIndex = 0;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            this.bt0.Click += new System.EventHandler(this.btGroupBox1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.btBackSpace);
+            this.groupBox2.Controls.Add(this.bt_result);
+            this.groupBox2.Controls.Add(this.bt_div);
+            this.groupBox2.Controls.Add(this.bt_multi);
+            this.groupBox2.Controls.Add(this.bt_minus);
+            this.groupBox2.Controls.Add(this.bt_plus);
+            this.groupBox2.Controls.Add(this.btCE);
+            this.groupBox2.Location = new System.Drawing.Point(494, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(241, 327);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // btBackSpace
+            // 
+            this.btBackSpace.Location = new System.Drawing.Point(50, 35);
+            this.btBackSpace.Name = "btBackSpace";
+            this.btBackSpace.Size = new System.Drawing.Size(52, 46);
+            this.btBackSpace.TabIndex = 15;
+            this.btBackSpace.Text = "<-";
+            this.btBackSpace.UseVisualStyleBackColor = true;
+            this.btBackSpace.Click += new System.EventHandler(this.BackSpace);
+            // 
+            // bt_result
+            // 
+            this.bt_result.Location = new System.Drawing.Point(50, 254);
+            this.bt_result.Name = "bt_result";
+            this.bt_result.Size = new System.Drawing.Size(153, 47);
+            this.bt_result.TabIndex = 14;
+            this.bt_result.Text = "=";
+            this.bt_result.UseVisualStyleBackColor = true;
+            this.bt_result.Click += new System.EventHandler(this.GetResult);
+            // 
+            // bt_div
+            // 
+            this.bt_div.Location = new System.Drawing.Point(151, 180);
+            this.bt_div.Name = "bt_div";
+            this.bt_div.Size = new System.Drawing.Size(52, 46);
+            this.bt_div.TabIndex = 13;
+            this.bt_div.Text = "/";
+            this.bt_div.UseVisualStyleBackColor = true;
+            this.bt_div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // bt_multi
+            // 
+            this.bt_multi.Location = new System.Drawing.Point(50, 180);
+            this.bt_multi.Name = "bt_multi";
+            this.bt_multi.Size = new System.Drawing.Size(52, 46);
+            this.bt_multi.TabIndex = 12;
+            this.bt_multi.Text = "*";
+            this.bt_multi.UseVisualStyleBackColor = true;
+            this.bt_multi.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // bt_minus
+            // 
+            this.bt_minus.Location = new System.Drawing.Point(151, 107);
+            this.bt_minus.Name = "bt_minus";
+            this.bt_minus.Size = new System.Drawing.Size(52, 46);
+            this.bt_minus.TabIndex = 11;
+            this.bt_minus.Text = "-";
+            this.bt_minus.UseVisualStyleBackColor = true;
+            this.bt_minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // bt_plus
+            // 
+            this.bt_plus.Location = new System.Drawing.Point(50, 107);
+            this.bt_plus.Name = "bt_plus";
+            this.bt_plus.Size = new System.Drawing.Size(52, 46);
+            this.bt_plus.TabIndex = 10;
+            this.bt_plus.Text = "+";
+            this.bt_plus.UseVisualStyleBackColor = true;
+            this.bt_plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // btCE
+            // 
+            this.btCE.Location = new System.Drawing.Point(151, 35);
+            this.btCE.Name = "btCE";
+            this.btCE.Size = new System.Drawing.Size(52, 47);
+            this.btCE.TabIndex = 0;
+            this.btCE.Text = "CE";
+            this.btCE.UseVisualStyleBackColor = true;
+            this.btCE.Click += new System.EventHandler(this.btClearAll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(625, 436);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Design By Wayne";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 460);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Label01);
+            this.Controls.Add(this.TextBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SimpleCalculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.Label Label01;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt_point;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button bt_result;
+        private System.Windows.Forms.Button bt_div;
+        private System.Windows.Forms.Button bt_multi;
+        private System.Windows.Forms.Button bt_minus;
+        private System.Windows.Forms.Button bt_plus;
+        private System.Windows.Forms.Button btCE;
+        private System.Windows.Forms.Button btBackSpace;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SimpleCalculatorForm/SimpleCalculatorForm/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculatorForm
+{
+    public partial class Form1 : Form
+    {
+        private List<double> _valueList = new List<double>();//_valueList存用户输入的数字
+        private List<string> _opertorList = new List<string>();//_opertorList存操作符
+        private bool _plusFlag = false;//+按下
+        private bool _minusFlag = false;//-按下
+        private bool _multiFlag = false;//×按下
+        private bool _divFlag = false;//÷按下
+        private bool _resultFlag = false;//=按下
+        private bool _canCalculateFlag = false; //可以计算
+        //private bool _backSpaceFlag = false; //按下回撤
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void NumDown(string str)
+        {
+
+            if (_plusFlag || _minusFlag || _multiFlag || _divFlag)
+            {
+                TextBox1.Clear();
+                _plusFlag = false;
+                _minusFlag = false;
+                _multiFlag = false;
+                _divFlag = false;
+                _resultFlag = false;
+                _canCalculateFlag = true;
+            }
+
+            if ((str.Equals(".") && TextBox1.Text.IndexOf(".") < 0) || (!str.Equals(".")))
+            {
+                if (_resultFlag)
+                {
+                    TextBox1.Clear();
+                    Label01.Text = TextBox1.Text;
+                    _resultFlag = false;
+                }
+                TextBox1.Text += str;
+                Label01.Text += str;
+            }
+
+        }
+
+        private void btGroupBox1_Click(object sender, EventArgs e)
+        {
+            NumDown(((Button)sender).Text);
+            //textBox1.Text = ((Button) sender).Text;
+        }
+
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            if ((!_plusFlag) && (!_minusFlag) && (!_multiFlag) && (!_divFlag) && (TextBox1.Text.Length > 0)&&(!TextBox1.Text.Equals(".")))
+            {
+                _resultFlag = false;
+                _valueList.Add(double.Parse(TextBox1.Text));
+                _opertorList.Add(((Button)sender).Text);
+                Label01.Text += ((Button)sender).Text;
+                _plusFlag = true;
+                TextBox1.Clear();
+                _canCalculateFlag = false;
+            }
+        }
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if ((!_plusFlag) && (!_minusFlag) && (!_multiFlag) && (!_divFlag)&&(TextBox1.Text.Length > 0) && (!TextBox1.Text.Equals(".")))
+            {
+
+                _resultFlag = false;
+                _valueList.Add(double.Parse(TextBox1.Text));
+                _opertorList.Add(((Button)sender).Text);
+                Label01.Text += ((Button)sender).Text;
+                _minusFlag = true;
+                TextBox1.Clear();
+                _canCalculateFlag = false;
+            }
+        }
+        private void mul_Click(object sender, EventArgs e)
+        {
+            if ((!_plusFlag) && (!_minusFlag) && (!_multiFlag) && (!_divFlag) && (TextBox1.Text.Length > 0) && (!TextBox1.Text.Equals(".")))
+            {
+                _resultFlag = false;
+                _valueList.Add(double.Parse(TextBox1.Text));
+                _opertorList.Add(((Button)sender).Text);
+                Label01.Text += ((Button)sender).Text;
+                _multiFlag = true;
+                TextBox1.Clear();
+                _canCalculateFlag = false;
+            }
+        }
+        private void div_Click(object sender, EventArgs e)
+        {
+            if ((!_plusFlag) && (!_minusFlag) && (!_multiFlag) && (!_divFlag) && (TextBox1.Text.Length > 0) && (!TextBox1.Text.Equals(".")))
+            {
+                _resultFlag = false;
+                _valueList.Add(double.Parse(TextBox1.Text));
+                _opertorList.Add(((Button)sender).Text);
+                Label01.Text += ((Button)sender).Text;
+                _divFlag = true;
+                TextBox1.Clear();
+                _canCalculateFlag = false;
+            }
+        }
+
+        private void GetResult(object sender, EventArgs e)
+        {
+            if ((_valueList.Count() > 0) && (_opertorList.Count() > 0) && (_canCalculateFlag == true) && (!TextBox1.Text.Equals(".")))
+            {
+                _valueList.Add(double.Parse(TextBox1.Text));
+                double Total = _valueList[0];
+                for (int i = 0; i < _opertorList.Count(); i++)
+                {
+                    string _operator = _opertorList[i];
+                    switch (_operator)
+                    {
+                        case "+":
+                            Total += _valueList[i + 1];
+                            break;
+                        case "-":
+                            Total -= _valueList[i + 1];
+                            break;
+                        case "*":
+                            Total *= _valueList[i + 1];
+                            break;
+                        case "/":
+                            {
+                                if (_valueList[i + 1] == 0)
+                                {
+                                    TextBox1.Text = "0 can not be divisor!Please try again!";
+                                    Label01.Text = "0 can not be divisor!Please try again!";
+                                    _opertorList.Clear();
+                                    _valueList.Clear();
+                                    _resultFlag = true;
+                                    return;
+                                }
+                                Total /= _valueList[i + 1];
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+
+                }
+                TextBox1.Text = Total.ToString();
+                Label01.Text = Total.ToString();
+                _opertorList.Clear();
+                _valueList.Clear();
+                _resultFlag = true;
+
+
+            }
+        }
+
+        private void btClearAll(object sender, EventArgs e)
+        {
+            _valueList.Clear();
+            _opertorList.Clear();
+            TextBox1.Text = "";
+            Label01.Text = "";
+            _plusFlag = false;
+            _minusFlag = false;
+            _multiFlag = false;
+            _divFlag = false;
+            _resultFlag = false;
+            _canCalculateFlag = false;
+        }
+
+        private void BackSpace(object sender, EventArgs e)
+        {
+            if (!_resultFlag && TextBox1.Text.Length>0)
+            {   
+
+                string TextBox1New = TextBox1.Text.Substring(0, TextBox1.Text.Length - 1);
+                string Label01New = Label01.Text.Substring(0, Label01.Text.Length - 1);
+                
+                TextBox1.Text = TextBox1New;
+                Label01.Text = Label01New;
+                if (TextBox1.Text.Length == 0)
+                {
+                    _canCalculateFlag = false;
+
+                }
+                else
+                {
+                    _canCalculateFlag = true;
+                }
+                //_valueList.RemoveAt(_valueList.Count()-1);
+                //_opertorList.RemoveAt(_opertorList.Count()-1);
+
+                //List<double> _valueListNew = 
+                //List<string> _opertorListNew = new List<string>();//_opertorList存操作符
+
+            }
+        }
+    }
+}
